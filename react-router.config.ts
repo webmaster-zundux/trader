652fd09a2569286cd71c7e5d9617ca08ac768cb2
webmaster-zundux <webmaster.zundux@gmail.com>
2025-08-09 15:00:07 +0700

    v0.0.1 build 5

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,12 +4,12 @@ import { PAGE_SLUG_HISTORY, PAGE_SLUG_INVENTORY, PAGE_SLUG_LOCATIONS, PAGE_SLUG_
 export default {
   ssr: false,
   prerender: [
-    `/${PAGE_SLUG_MARKET}`,
-    `/${PAGE_SLUG_HISTORY}`,
-    `/${PAGE_SLUG_INVENTORY}`,
-    `/${PAGE_SLUG_LOCATIONS}`,
-    `/${PAGE_SLUG_MOVING_ENTITIES}`,
-    `/${PAGE_SLUG_MAP}`,
-    `/${PAGE_SLUG_PRODUCTS}`,
+    `${import.meta.env.BASE_URL}${PAGE_SLUG_MARKET}`,
+    `${import.meta.env.BASE_URL}${PAGE_SLUG_HISTORY}`,
+    `${import.meta.env.BASE_URL}${PAGE_SLUG_INVENTORY}`,
+    `${import.meta.env.BASE_URL}${PAGE_SLUG_LOCATIONS}`,
+    `${import.meta.env.BASE_URL}${PAGE_SLUG_MOVING_ENTITIES}`,
+    `${import.meta.env.BASE_URL}${PAGE_SLUG_MAP}`,
+    `${import.meta.env.BASE_URL}${PAGE_SLUG_PRODUCTS}`,
   ],
 } satisfies Config
