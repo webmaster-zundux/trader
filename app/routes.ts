ba732c9d6ea6b61555bf0619be0a791983aa912c
webmaster-zundux <webmaster.zundux@gmail.com>
2025-08-09 15:13:45 +0700

    v0.0.1 build 6

--- app/routes.ts
+++ app/routes.ts
@@ -2,15 +2,14 @@ import { type RouteConfig, index, prefix, route } from '@react-router/dev/routes
 import { PAGE_SLUG_HISTORY, PAGE_SLUG_INVENTORY, PAGE_SLUG_LOCATIONS, PAGE_SLUG_MAP, PAGE_SLUG_MOVING_ENTITIES, PAGE_SLUG_PRODUCTS } from './router/PageSlugs.const'
 
 export default [
-  ...prefix(`${import.meta.env.BASE_URL || '/'}`,
-    [
-      index('routes/market.tsx'),
-      route(PAGE_SLUG_INVENTORY, 'routes/inventory.tsx'),
-      route(PAGE_SLUG_PRODUCTS, 'routes/products.tsx'),
-      route(PAGE_SLUG_LOCATIONS, 'routes/locations.tsx'),
-      route(PAGE_SLUG_MOVING_ENTITIES, 'routes/movingEntities.tsx'),
-      route(PAGE_SLUG_MAP, 'routes/map.tsx'),
-      route(PAGE_SLUG_HISTORY, 'routes/history.tsx'),
-    ]),
+  // ...prefix(`${import.meta.env.BASE_URL || '/'}`, [
+  index('routes/market.tsx'),
+  route(PAGE_SLUG_INVENTORY, 'routes/inventory.tsx'),
+  route(PAGE_SLUG_PRODUCTS, 'routes/products.tsx'),
+  route(PAGE_SLUG_LOCATIONS, 'routes/locations.tsx'),
+  route(PAGE_SLUG_MOVING_ENTITIES, 'routes/movingEntities.tsx'),
+  route(PAGE_SLUG_MAP, 'routes/map.tsx'),
+  route(PAGE_SLUG_HISTORY, 'routes/history.tsx'),
+  // ]),
   route('*?', 'routes/404.tsx'),
 ] satisfies RouteConfig
