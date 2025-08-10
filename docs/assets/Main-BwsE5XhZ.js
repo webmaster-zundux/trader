f5c443a6707451e4c9a819ae9ec0bb372d45e773
webmaster-zundux <webmaster.zundux@gmail.com>
2025-08-10 21:31:31 +0700

    add 404.html for gh-pages by copy content from index.html

--- docs/assets/Main-BwsE5XhZ.js
+++
@@ -1,1 +0,0 @@
-import{a as n,p as s}from"./chunk-NL6KNZEE-Dk3Cym_s.js";const t="_Main_g1bqu_1",i={Main:t},e=n.memo(function({children:a}){return s.jsx("main",{className:i.Main,children:a})});export{e as M};
