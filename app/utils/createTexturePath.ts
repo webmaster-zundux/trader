39f72a378e5dc6fcfc84bb5cd0e5effbe71c2bcb
webmaster-zundux <webmaster.zundux@gmail.com>
2025-08-11 14:08:42 +0700

    add usage BASE_URL into a sprite path creation

--- app/utils/createTexturePath.ts
+++ app/utils/createTexturePath.ts
@@ -3,5 +3,5 @@ const MAP_TEXTURE_FOLDER_PATH = `/images/textures`
 export function createTexturePath(
   textureName: string
 ) {
-  return `${MAP_TEXTURE_FOLDER_PATH}/${textureName}.png`
+  return `${import.meta.env.BASE_URL}${MAP_TEXTURE_FOLDER_PATH}/${textureName}.png`
 }
